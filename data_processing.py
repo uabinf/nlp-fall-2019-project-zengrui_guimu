2bebf33f4a28940915096333ad197cdafc48cfea
ElementJet <zengrui.liu@gmail.com>
2019-11-20 23:21:36 -0600

    Process two dataset. Save sentences in array and characters in set
    Save sentences in arrays and characters in sets.

---
+++ data_processing.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+batch_size = 64
+epochs = 100
+latent_dim = 256
+num_samples = 20000
+
+data_path_input='modern.txt'
+data_path_output='original.txt'
+
+input_texts = []
+target_texts = []
+input_characters = set()
+target_characters = set()
+
+with open(data_path_input, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    input_texts.append(line)
+    for char in line:
+        if char not in input_characters:
+            input_characters.add(char)
+
+with open(data_path_output, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    target=line.split('\n')
+    target_texts.append(target[0])
+    line = '\t' + line + '\n'    
+    for char in line:
+        if char not in target_characters:
+            target_characters.add(char)
+
+input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters)
+num_decoder_tokens = len(target_characters)
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+
+input_token_index = dict(
+    [(char, i) for i, char in enumerate(input_characters)])
+target_token_index = dict(
+    [(char, i) for i, char in enumerate(target_characters)])
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    encoder_input_data[i, t + 1:, input_token_index[' ']] = 1.
+    for t, char in enumerate(target_text):
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+    decoder_input_data[i, t + 1:, target_token_index[' ']] = 1.
+    decoder_target_data[i, t:, target_token_index[' ']] = 1.
